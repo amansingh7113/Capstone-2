d81b308688f9be50a54b6e50eb9c55008461dd6d
Aman Singh <amansingh71130@gmail.com>
2022-11-01 14:08:28 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using LoginAPI.Models;
+using Microsoft.AspNetCore.Identity;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http.ModelBinding;
+
+namespace LoginAPI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(
+                    model.Username, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+            
+
+                ModelState.AddModelError(string.Empty, "Your email and password combination are incorrect");
+            }
+
+            return View(model);
+        
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginAPI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
